cff03b40e4b15231340a7ec83f67a7d6c7c9335e
am-gooru <ashish@gooru.org>
2016-05-09 16:48:21 +0530

    Startup hardening

--- src/main/java/org/gooru/nucleus/handlers/copier/bootstrap/CopierVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/copier/bootstrap/CopierVerticle.java
@@ -1,10 +1,5 @@
 package org.gooru.nucleus.handlers.copier.bootstrap;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.json.JsonObject;
-
 import org.gooru.nucleus.handlers.copier.bootstrap.shutdown.Finalizer;
 import org.gooru.nucleus.handlers.copier.bootstrap.shutdown.Finalizers;
 import org.gooru.nucleus.handlers.copier.bootstrap.startup.Initializer;
@@ -16,6 +11,11 @@ import org.gooru.nucleus.handlers.copier.processors.responses.MessageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+
 public class CopierVerticle extends AbstractVerticle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CopierVerticle.class);
@@ -23,50 +23,49 @@ public class CopierVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> voidFuture) throws Exception {
 
+        EventBus eb = vertx.eventBus();
         vertx.executeBlocking(blockingFuture -> {
             startApplication();
             blockingFuture.complete();
-        } , future -> {
-            if (future.succeeded()) {
-                voidFuture.complete();
+        }, startApplicationFuture -> {
+            if (startApplicationFuture.succeeded()) {
+                eb.consumer(MessagebusEndpoints.MBEP_COPIER, message -> {
+                    LOGGER.debug("Received message: " + message.body());
+                    vertx.executeBlocking(future -> {
+                        MessageResponse result = new ProcessorBuilder(message).build().process();
+                        future.complete(result);
+                    }, res -> {
+                        MessageResponse result = (MessageResponse) res.result();
+                        message.reply(result.reply(), result.deliveryOptions());
+
+                        JsonObject eventData = result.event();
+                        if (eventData != null) {
+                            String sessionToken =
+                                ((JsonObject) message.body()).getString(MessageConstants.MSG_HEADER_TOKEN);
+                            if (sessionToken != null && !sessionToken.isEmpty()) {
+                                eventData.put(MessageConstants.MSG_HEADER_TOKEN, sessionToken);
+                            } else {
+                                LOGGER.warn("Invalid session token received");
+                            }
+                            eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+                        }
+                    });
+
+                }).completionHandler(result -> {
+                    if (result.succeeded()) {
+                        LOGGER.info("Copier end point ready to listen");
+                        voidFuture.complete();
+                    } else {
+                        LOGGER.error("Error registering the copier handler. Halting the Copier machinery");
+                        voidFuture.fail(result.cause());
+                        Runtime.getRuntime().halt(1);
+                    }
+                });
             } else {
                 voidFuture.fail("Not able to initialize the Copier machinery properly");
             }
         });
 
-        EventBus eb = vertx.eventBus();
-
-        eb.consumer(MessagebusEndpoints.MBEP_COPIER, message -> {
-
-            LOGGER.debug("Received message: " + message.body());
-
-            vertx.executeBlocking(future -> {
-                MessageResponse result = new ProcessorBuilder(message).build().process();
-                future.complete(result);
-            } , res -> {
-                MessageResponse result = (MessageResponse) res.result();
-                message.reply(result.reply(), result.deliveryOptions());
-
-                JsonObject eventData = result.event();
-                if (eventData != null) {
-                    String sessionToken = ((JsonObject) message.body()).getString(MessageConstants.MSG_HEADER_TOKEN);
-                    if (sessionToken != null && !sessionToken.isEmpty()) {
-                        eventData.put(MessageConstants.MSG_HEADER_TOKEN, sessionToken);
-                    } else {
-                        LOGGER.warn("Invalid session token received");
-                    }
-                    eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
-                }
-            });
-
-        }).completionHandler(result -> {
-            if (result.succeeded()) {
-                LOGGER.info("Copier end point ready to listen");
-            } else {
-                LOGGER.error("Error registering the copier handler. Halting the Copier machinery");
-                Runtime.getRuntime().halt(1);
-            }
-        });
     }
 
     @Override
