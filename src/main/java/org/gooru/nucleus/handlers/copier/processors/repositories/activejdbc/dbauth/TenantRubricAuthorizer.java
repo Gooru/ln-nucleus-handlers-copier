86ffcddb38dfb89b6a492eae3eae167ba2ebbeeb
szgooru <sachin@gooru.org>
2017-03-09 10:48:44 +0530

    updating authorizer for tenant authrize

--- src/main/java/org/gooru/nucleus/handlers/copier/processors/repositories/activejdbc/dbauth/TenantRubricAuthorizer.java
+++ src/main/java/org/gooru/nucleus/handlers/copier/processors/repositories/activejdbc/dbauth/TenantRubricAuthorizer.java
@@ -1,9 +1,17 @@
 package org.gooru.nucleus.handlers.copier.processors.repositories.activejdbc.dbauth;
 
+import java.util.ResourceBundle;
+
 import org.gooru.nucleus.handlers.copier.processors.ProcessorContext;
 import org.gooru.nucleus.handlers.copier.processors.repositories.activejdbc.entities.AJEntityRubric;
 import org.gooru.nucleus.handlers.copier.processors.responses.ExecutionResult;
 import org.gooru.nucleus.handlers.copier.processors.responses.MessageResponse;
+import org.gooru.nucleus.handlers.copier.processors.responses.MessageResponseFactory;
+import org.gooru.nucleus.libs.tenant.TenantTree;
+import org.gooru.nucleus.libs.tenant.TenantTreeBuilder;
+import org.gooru.nucleus.libs.tenant.contents.ContentTenantAuthorization;
+import org.gooru.nucleus.libs.tenant.contents.ContentTenantAuthorizationBuilder;
+import org.gooru.nucleus.libs.tenant.contents.ContentTreeAttributes;
 
 /**
  * @author szgooru
@@ -12,6 +20,7 @@ import org.gooru.nucleus.handlers.copier.processors.responses.MessageResponse;
 public class TenantRubricAuthorizer implements Authorizer<AJEntityRubric> {
 
     private final ProcessorContext context;
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("messages");
     
     public TenantRubricAuthorizer(ProcessorContext context) {
         this.context = context;
@@ -19,7 +28,18 @@ public class TenantRubricAuthorizer implements Authorizer<AJEntityRubric> {
 
     @Override
     public ExecutionResult<MessageResponse> authorize(AJEntityRubric model) {
-        return new ExecutionResult<>(null, ExecutionResult.ExecutionStatus.CONTINUE_PROCESSING);
+        TenantTree userTenantTree = TenantTreeBuilder.build(context.tenant(), context.tenantRoot());
+        TenantTree contentTenantTree = TenantTreeBuilder.build(model.getTenant(), model.getTenantRoot());
+
+        ContentTenantAuthorization authorization = ContentTenantAuthorizationBuilder
+            .build(contentTenantTree, userTenantTree, ContentTreeAttributes.build(model.isPublished()));
+
+        if (authorization.canRead()) {
+            return new ExecutionResult<>(null, ExecutionResult.ExecutionStatus.CONTINUE_PROCESSING);
+        }
+        return new ExecutionResult<>(
+            MessageResponseFactory.createNotFoundResponse(RESOURCE_BUNDLE.getString("not.found")),
+            ExecutionResult.ExecutionStatus.FAILED);
     }
 
 }
--- src/main/java/org/gooru/nucleus/handlers/copier/processors/repositories/activejdbc/entities/AJEntityRubric.java
+++ src/main/java/org/gooru/nucleus/handlers/copier/processors/repositories/activejdbc/entities/AJEntityRubric.java
@@ -9,6 +9,12 @@ import org.javalite.activejdbc.annotations.Table;
 @Table("rubric")
 public class AJEntityRubric extends Model {
     
+    private static final String TENANT = "tenant";
+    private static final String TENANT_ROOT = "tenant_root";
+
+    private static final String PUBLISH_STATUS = "publish_status";
+    private static final String PUBLISH_STATUS_PUBLISHED = "published";
+    
     public static final String AUTHORIZER_QUERY = "id = ?::uuid and is_deleted = false";
     
     public static final String COPY_RUBRIC =
@@ -19,4 +25,17 @@ public class AJEntityRubric extends Model {
         + " coalesce(original_creator_id,creator_id) as original_creator_id, coalesce(original_rubric_id,?::uuid) as original_rubric_id, ?::uuid, publish_date,"
         + " publish_status, metadata, taxonomy, gut_codes, thumbnail, created_at, updated_at, ?::uuid, ?::uuid, visible_on_profile, is_deleted,"
         + " creator_system FROM rubric WHERE id = ?::uuid AND is_deleted = false";
+    
+    public String getTenant() {
+        return this.getString(TENANT);
+    }
+
+    public String getTenantRoot() {
+        return this.getString(TENANT_ROOT);
+    }
+
+    public boolean isPublished() {
+        String publishStatus = this.getString(PUBLISH_STATUS);
+        return PUBLISH_STATUS_PUBLISHED.equalsIgnoreCase(publishStatus);
+    }
 }
